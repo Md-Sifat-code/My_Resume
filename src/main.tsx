e49513a5b0ac62000b2f7cc54fd445f143e505ac
Md-Sifat-code <codewithsifat4@gmail.com>
2025-02-06 00:18:22 +0600

    initial set up done

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+// Create a router with routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+// Find the root element and render the RouterProvider
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+}
